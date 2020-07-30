416ef50c9ac62fd97479e70c8c2e6724f0a64200
Sharad Choudhary <isharad2211@gmail.com>
2020-07-30 23:47:42 +0530

    Updates

---
+++ precache-manifest.14e60744d240a5d8eeb9899d73557f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee939ac6baeed8e36af544963c76f5c0",
+    "url": "/deploy-crm-kabs-demo/index.html"
+  },
+  {
+    "revision": "86089856c7b9a1638e86",
+    "url": "/deploy-crm-kabs-demo/static/js/2.7fe899a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deploy-crm-kabs-demo/static/js/2.7fe899a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a46899080c49ab88f4",
+    "url": "/deploy-crm-kabs-demo/static/js/main.cf5221f1.chunk.js"
+  },
+  {
+    "revision": "a3616cecf5a495e14d01",
+    "url": "/deploy-crm-kabs-demo/static/js/runtime-main.17b733c8.js"
+  },
+  {
+    "revision": "ad8a12c115fa73d4b87417b378f8e69e",
+    "url": "/deploy-crm-kabs-demo/static/media/yearbook.ad8a12c1.svg"
+  }
+]);
